efef4b6a7605f80c3967711499b3708f01e6c890
philippschulte <philipp.schulte@ymail.com>
2016-08-29 22:23:11 -0700

    Add /forecast endpoint to make weather request from server

--- app/api/OpenWeatherMap.jsx
+++ app/api/OpenWeatherMap.jsx
@@ -1,12 +1,11 @@
 var axios = require('axios');
 
-const API_KEY = process.env.API_KEY;
-const OPEN_WEATHER_MAP_URL = `http://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}&units=metric`;
+const API_URL = 'http://salty-caverns-63765.herokuapp.com';
 
 module.exports = {
   getTemp: function (location) {
     var encodedLocation = encodeURIComponent(location);
-    var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+    var requestUrl = `${API_URL}/forecast/${encodedLocation}`;
 
     return axios.get(requestUrl)
       .then(function (res) {
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 // Dependencies:
 var express = require('express');
+var cors = require('cors');
+var axios = require('axios');
 
 // Express:
 var app = express();
@@ -13,10 +15,29 @@ app.use(function (req, res, next) {
 	}
 });
 app.use(express.static('public'));
+app.use('/', cors());
 
-// Port:
-const PORT = process.env.PORT;
-console.log('process.env.API_KEY: ', process.env.API_KEY);
+// Config Variables:
+const PORT = process.env.PORT || 3000;
+const API_KEY = process.env.API_KEY;
+
+// OpenWeatherMap API URL
+const OPEN_WEATHER_MAP_URL = `http://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}&units=metric`;
+
+// API:
+app.get('/forecast/:location', function(req, res) {
+	var location = req.params.location;
+  var encodedLocation = encodeURIComponent(location);
+  var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+
+  axios.get(requestUrl)
+    .then(function (res) {
+			res.json(res);
+    })
+    .catch(function (err) {
+      res.status(500).send(err);
+    });
+});
 
 // Starting server:
 app.listen(PORT, function (err) {
